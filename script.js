5dfd0f6058de42d22cdcde63cd58c3116c4d8a15
josman227 <josmutinhiri@gmail.com>
2023-06-27 19:34:17 +0200

    Mobile Menu Make Changes

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ const cardsSelect = document.getElementById('cards');
 const aboutSelect = document.getElementById('about-section');
 const contactSelect = document.getElementById('contact-me');
 const mobileMenu = document.querySelector('.mobile-links');
-const mobileLink = document.querySelector('.link');
-const mobileLink2 = document.querySelector('.link2');
-const mobileLink3 = document.querySelector('.link3');
+const mobileLinks = document.querySelectorAll('.mobile-link');
 
 mobileMenu.classList.add('hideit');
 
@@ -41,38 +39,16 @@ cancelSelect.addEventListener('click', () => {
   contactSelect.classList.remove('blur');
 });
 
-mobileLink.addEventListener('click', () => {
-  navSelect.classList.remove('blur');
-  bioSelect.classList.remove('blur');
-  cardsSelect.classList.remove('blur');
-  aboutSelect.classList.remove('blur');
-  contactSelect.classList.remove('blur');
-  logoSelect.classList.remove('blur');
-  mobileMenu.classList.add('hideit');
-  cancelSelect.classList.add('hideit');
-  menuSelect.classList.remove('hideit');
-});
-
-mobileLink2.addEventListener('click', () => {
-  navSelect.classList.remove('blur');
-  bioSelect.classList.remove('blur');
-  cardsSelect.classList.remove('blur');
-  aboutSelect.classList.remove('blur');
-  contactSelect.classList.remove('blur');
-  logoSelect.classList.remove('blur');
-  mobileMenu.classList.add('hideit');
-  cancelSelect.classList.add('hideit');
-  menuSelect.classList.remove('hideit');
-});
-
-mobileLink3.addEventListener('click', () => {
-  navSelect.classList.remove('blur');
-  bioSelect.classList.remove('blur');
-  cardsSelect.classList.remove('blur');
-  aboutSelect.classList.remove('blur');
-  contactSelect.classList.remove('blur');
-  logoSelect.classList.remove('blur');
-  mobileMenu.classList.add('hideit');
-  cancelSelect.classList.add('hideit');
-  menuSelect.classList.remove('hideit');
+mobileLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navSelect.classList.remove('blur');
+    bioSelect.classList.remove('blur');
+    cardsSelect.classList.remove('blur');
+    aboutSelect.classList.remove('blur');
+    contactSelect.classList.remove('blur');
+    logoSelect.classList.remove('blur');
+    mobileMenu.classList.add('hideit');
+    cancelSelect.classList.add('hideit');
+    menuSelect.classList.remove('hideit');
+  });
 });
