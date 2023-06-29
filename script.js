57082c11e72d5bb1aaeb1492da2bd48178987ad7
Ghayoor Ul Baqir <122298629+Xghayor@users.noreply.github.com>
2023-06-29 16:35:20 -0700

    Add javascript for Popup-window

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/* eslint-disable quotes */
 // Mobile menu
 const menuSelect = document.querySelector('.menu');
 const logoSelect = document.querySelector('.logo');
@@ -10,9 +9,7 @@ const aboutSelect = document.getElementById('about-section');
 const contactSelect = document.getElementById('contact-me');
 const mobileMenu = document.querySelector('.mobile-links');
 const mobileLinks = document.querySelectorAll('.mobile-link');
-
 mobileMenu.classList.add('hideit');
-
 function blurry() {
   navSelect.classList.toggle('blur');
   bioSelect.classList.toggle('blur');
@@ -21,14 +18,12 @@ function blurry() {
   contactSelect.classList.toggle('blur');
   logoSelect.classList.toggle('blur');
 }
-
 menuSelect.addEventListener('click', () => {
   menuSelect.classList.add('hideit');
   cancelSelect.style.display = 'block';
   mobileMenu.classList.remove('hideit');
   blurry();
 });
-
 cancelSelect.addEventListener('click', () => {
   menuSelect.classList.remove('hideit');
   cancelSelect.style.display = 'none';
@@ -39,7 +34,6 @@ cancelSelect.addEventListener('click', () => {
   aboutSelect.classList.remove('blur');
   contactSelect.classList.remove('blur');
 });
-
 mobileLinks.forEach((link) => {
   link.addEventListener('click', () => {
     navSelect.classList.remove('blur');
@@ -53,10 +47,8 @@ mobileLinks.forEach((link) => {
     menuSelect.classList.remove('hideit');
   });
 });
-
 const showCard = () => {
   const cardContainer = document.querySelector('#cards');
-
   // Array of card data objects
   const cards = [
     {
@@ -101,12 +93,10 @@ const showCard = () => {
     },
     // Add more card objects as needed
   ];
-
   // eslint-disable-next-line no-unused-vars
   cards.forEach((card, index) => {
     const article = document.createElement('article');
     article.classList.add('card');
-
     const cardImg = document.createElement('div');
     cardImg.classList.add('card-img');
     const img = document.createElement('img');
@@ -114,34 +104,26 @@ const showCard = () => {
     img.alt = card.alt;
     cardImg.appendChild(img);
     article.appendChild(cardImg);
-
     const cardItems = document.createElement('div');
     cardItems.classList.add('card-items');
-
     const cardTitle = document.createElement('h2');
     cardTitle.textContent = card.name;
     cardItems.appendChild(cardTitle);
-
     const cardProps = document.createElement('ul');
     cardProps.classList.add('card-props');
-
     card.features.forEach((feature) => {
       const cardProp = document.createElement('li');
       cardProp.classList.add('card-prop');
       cardProp.textContent = feature;
       cardProps.appendChild(cardProp);
     });
-
     cardItems.appendChild(cardProps);
-
     const cardText = document.createElement('p');
     cardText.classList.add('card-text');
     cardText.textContent = card.description;
     cardItems.appendChild(cardText);
-
     const cardLanguages = document.createElement('ul');
     cardLanguages.classList.add('card-languages');
-
     card.languages.forEach((language) => {
       const cardLanguage = document.createElement('li');
       cardLanguage.classList.add('card-language');
@@ -150,18 +132,14 @@ const showCard = () => {
       cardLanguage.appendChild(p);
       cardLanguages.appendChild(cardLanguage);
     });
-
     cardItems.appendChild(cardLanguages);
-
     const cardButton = document.createElement('button');
     cardButton.classList.add('card-btn');
     cardButton.id = card.id;
     cardButton.textContent = 'See Project';
     cardItems.appendChild(cardButton);
-
     article.appendChild(cardItems);
     cardContainer.appendChild(article);
-
     // Event listener for card button click
     cardButton.addEventListener('click', () => {
       // eslint-disable-next-line no-use-before-define
@@ -171,46 +149,37 @@ const showCard = () => {
     });
   });
 };
-
 showCard();
-
 // Function to open popup window
 const openPopup = (card) => {
   const popup = document.createElement('div');
   popup.classList.add('popup');
-
   const closeButton = document.createElement('button');
   closeButton.classList.add('popup-close');
   closeButton.textContent = 'X';
-
   const popupContent = document.createElement('div');
   popupContent.classList.add('popup-content');
   popupContent.innerHTML = `
     <div class ="popup-items"></div>
     <h2>${card.name}</h2>
-     <ul class ="card-prop">${card.features}</ul>  
+     <ul class ="card-prop">${card.features}</ul>
     <img src="${card.img}" alt="${card.alt}">
     <p class ="pop-text">${card.description} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text <br><br> ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.</p>
     <ul class = "card-language">${card.languages}</ul>
     <button class = "pop-btn"><p>See live</p></button>
     <button class = "pop-btn"><p>See Source</p></button>
-
   `;
-
   popup.appendChild(closeButton);
   popup.appendChild(popupContent);
   document.body.appendChild(popup);
-
   // Event listener for close button click
   closeButton.addEventListener('click', () => {
     document.body.removeChild(popup);
     blurry();
   });
 };
-
 // Clientside Validation
 const userMail = document.querySelector('#userEmail');
-
 userMail.addEventListener('input', (event) => {
   const userInput = event.target.value.toLowerCase();
   if (event.target.value !== userInput) {
@@ -219,9 +188,7 @@ userMail.addEventListener('input', (event) => {
     userMail.setCustomValidity('');
   }
 });
-
 // Preserve User Data in Browser
-
 document.querySelectorAll('.usrInput').forEach((value) => {
   value.addEventListener('input', () => {
     let user = {
@@ -233,9 +200,7 @@ document.querySelectorAll('.usrInput').forEach((value) => {
     localStorage.setItem('user', user);
   });
 });
-
 // Preserve data in Browser
-
 window.addEventListener('load', () => {
   const userValue = localStorage.getItem('user');
   if (userValue) {
