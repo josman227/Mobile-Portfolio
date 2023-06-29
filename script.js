6ed43c9eb654aab04d22619b348fd89e96b36686
josman227 <josmutinhiri@gmail.com>
2023-06-29 19:34:18 +0200

    Add javascript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 // Mobile menu
 const menuSelect = document.querySelector('.menu');
 const logoSelect = document.querySelector('.logo');
@@ -52,3 +53,170 @@ mobileLinks.forEach((link) => {
     menuSelect.classList.remove('hideit');
   });
 });
+
+const showCard = () => {
+  const cardContainer = document.querySelector('#cards');
+
+  // Array of card data objects
+  const cards = [
+    {
+      img: 'img/work-3.png',
+      alt: 'Work 1',
+      name: 'Tonic',
+      features: ['Canopy', 'Back End Dev', '2015'],
+      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      languages: ['HTML', 'Ruby on Rails', 'CSS', 'JavaScript'],
+      id: 'card1-btn',
+      cardIndex: 0, // Add card index property
+    },
+    {
+      img: 'img/work-1.png',
+      alt: 'Work 2',
+      name: 'Multi-Post Stories',
+      features: ['FACEBOOK', 'Full Stack Dev', '2015'],
+      description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      languages: ['HTML', 'JavaScript'],
+      id: 'card2-btn',
+      cardIndex: 1, // Add card index property
+    },
+    {
+      img: 'img/work-1.png',
+      alt: 'Work 2',
+      name: 'Multi-Post Stories',
+      features: ['FACEBOOK', 'Full Stack Dev', '2015'],
+      description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      languages: ['HTML', 'JavaScript'],
+      id: 'card2-btn',
+      cardIndex: 2, // Add card index property
+    },
+    {
+      img: 'img/work-1.png',
+      alt: 'Work 2',
+      name: 'Multi-Post Stories',
+      features: ['FACEBOOK', 'Full Stack Dev', '2015'],
+      description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      languages: ['HTML', 'JavaScript'],
+      id: 'card2-btn',
+      cardIndex: 3, // Add card index property
+    },
+    // Add more card objects as needed
+  ];
+
+  // eslint-disable-next-line no-unused-vars
+  cards.forEach((card, index) => {
+    const article = document.createElement('article');
+    article.classList.add('card');
+
+    const cardImg = document.createElement('div');
+    cardImg.classList.add('card-img');
+    const img = document.createElement('img');
+    img.src = card.img;
+    img.alt = card.alt;
+    cardImg.appendChild(img);
+    article.appendChild(cardImg);
+
+    const cardItems = document.createElement('div');
+    cardItems.classList.add('card-items');
+
+    const cardTitle = document.createElement('h2');
+    cardTitle.textContent = card.name;
+    cardItems.appendChild(cardTitle);
+
+    const cardProps = document.createElement('ul');
+    cardProps.classList.add('card-props');
+
+    card.features.forEach((feature) => {
+      const cardProp = document.createElement('li');
+      cardProp.classList.add('card-prop');
+      cardProp.textContent = feature;
+      cardProps.appendChild(cardProp);
+    });
+
+    cardItems.appendChild(cardProps);
+
+    const cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    cardText.textContent = card.description;
+    cardItems.appendChild(cardText);
+
+    const cardLanguages = document.createElement('ul');
+    cardLanguages.classList.add('card-languages');
+
+    card.languages.forEach((language) => {
+      const cardLanguage = document.createElement('li');
+      cardLanguage.classList.add('card-language');
+      const p = document.createElement('p');
+      p.textContent = language;
+      cardLanguage.appendChild(p);
+      cardLanguages.appendChild(cardLanguage);
+    });
+
+    cardItems.appendChild(cardLanguages);
+
+    const cardButton = document.createElement('button');
+    cardButton.classList.add('card-btn');
+    cardButton.id = card.id;
+    cardButton.textContent = 'See Project';
+    cardItems.appendChild(cardButton);
+
+    article.appendChild(cardItems);
+    cardContainer.appendChild(article);
+
+    // Event listener for card button click
+    cardButton.addEventListener('click', () => {
+      // eslint-disable-next-line no-use-before-define
+      blurry();
+      // eslint-disable-next-line no-use-before-define
+      openPopup(card);
+    });
+  });
+};
+
+showCard();
+
+// Function to open popup window
+const openPopup = (card) => {
+  const popup = document.createElement('div');
+  popup.classList.add('popup');
+
+  const closeButton = document.createElement('button');
+  closeButton.classList.add('popup-close');
+  closeButton.textContent = 'X';
+
+  const popupContent = document.createElement('div');
+  popupContent.classList.add('popup-content');
+  popupContent.innerHTML = `
+    <h2>${card.name}</h2>
+     <ul class ="card-prop">${card.features}</ul>  
+    <img src="${card.img}" alt="${card.alt}">
+    <p class ="pop-text">${card.description} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text <br><br> ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.</p>
+    <ul class = "card-language">${card.languages}</ul>
+    <button class = "pop-btn"><p>See live</p></button>
+    <button class = "pop-btn"><p>See Source</p></button>
+
+  `;
+
+  popup.appendChild(closeButton);
+  popup.appendChild(popupContent);
+  document.body.appendChild(popup);
+
+  // Event listener for close button click
+  closeButton.addEventListener('click', () => {
+    document.body.removeChild(popup);
+    blurry();
+  });
+};
+
+// Clientside Validation
+const userMail = document.querySelector('#userEmail');
+
+userMail.addEventListener('input', (event) => {
+  const userInput = event.target.value.toLowerCase();
+  if (event.target.value !== userInput) {
+    userMail.setCustomValidity('Please enter your email address using lowercase letters.');
+  } else {
+    userMail.setCustomValidity('');
+  }
+});
+
+// Store Data in the Browser
